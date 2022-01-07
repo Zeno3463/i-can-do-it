70f173d06a368cd0ccf7ae367051644cd1ce356c
Zeno3463 <zenolim3463@gmail.com>
2022-01-08 07:03:38 +0800

    feat: Added functionality for get_random_video()

--- server/functions.py
+++ server/functions.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import random
-from urllib.parse import urlparse
 
 def get_random_img(num):
 	sources = [
@@ -14,14 +13,15 @@ def get_random_img(num):
 
 def get_random_video(num):
 	sources = [
-		"https://www.google.com/search?q=inspirational+speeches&rlz=1C1CHBD_enMY902MY902&sxsrf=AOaemvK_dsm36aC-Bvkuvjd8nuPMJ3ZPFA:1641517992740&source=lnms&tbm=vid&sa=X&sqi=2&ved=2ahUKEwiIweWGu571AhXs-SoKHc-2D9EQ_AUoAXoECAEQAw&biw=1280&bih=601&dpr=1.5"
+		"https://www.google.com/search?q=inspirational+speeches&rlz=1C1CHBD_enMY902MY902&sxsrf=AOaemvK_dsm36aC-Bvkuvjd8nuPMJ3ZPFA:1641517992740&source=lnms&tbm=vid&sa=X&sqi=2&ved=2ahUKEwiIweWGu571AhXs-SoKHc-2D9EQ_AUoAXoECAEQAw&biw=1280&bih=601&dpr=1.5",
+		"https://www.google.com/search?q=motivational+videos&rlz=1C1CHBD_enMY902MY902&biw=1280&bih=601&tbm=vid&sxsrf=AOaemvLqSf_HNIYH8e6aROO23bZbn8rh-Q%3A1641596424815&ei=CMbYYceSMb2_3LUPusWNwAo&ved=0ahUKEwjHto-e36D1AhW9H7cAHbpiA6gQ4dUDCA4&uact=5&oq=motivational+videos&gs_lcp=Cg1nd3Mtd2l6LXZpZGVvEAMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBAgAEAoyBAgAEAoyBQgAEIAEMgUIABCABDoECCMQJzoHCCMQ6gIQJzoECAAQQzoLCAAQgAQQsQMQgwE6CAgAEIAEELEDOggIABCxAxCDAToHCAAQsQMQQzoHCAAQgAQQCjoFCAAQkQJQ9QZY9DtgjT1oAnAAeACAAUqIAY8JkgECMjGYAQCgAQGwAQrAAQE&sclient=gws-wiz-video",
+		"https://www.google.com/search?q=celebrity+motivational+speeches&rlz=1C1CHBD_enMY902MY902&biw=1280&bih=601&tbm=vid&sxsrf=AOaemvKd3Ie7V4RTCFwtc3OfGwO7zsCkPg%3A1641596436401&ei=FMbYYerrF4bWz7sPvdSoiAY&oq=celebrity+motiv&gs_lcp=Cg1nd3Mtd2l6LXZpZGVvEAMYADIKCAAQgAQQhwIQFDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyCggAEIAEEIcCEBQyBQgAEIAEOgQIIxAnOgUIABCRAjoLCAAQgAQQsQMQgwE6CAgAELEDEIMBOggIABCABBCxAzoECAAQQzoFCAAQsQNQAFjbHGDbKWgAcAB4AIABW4gBkAeSAQIxNZgBAKABAcABAQ&sclient=gws-wiz-video"
 	]
 	website = requests.get(random.choice(sources))
 	soup = BeautifulSoup(website.content, 'html.parser')
-	for a in soup.find_all('a'):
-		if a.get('href') and 'youtube' in a.get('href'):
-			print(a.get('href'))
-	return ""
+	yt_links = [a.get('href') for a in soup.find_all('a', href=True) if 'youtube' in a.get('href')]
+	pingless_yt_links = [f"https://www.youtube.com/watch?v={a.replace('/url?q=https://www.youtube.com/watch%3Fv%3D', '').split('&sa')[0]}" for a in yt_links]
+	return random.choices(pingless_yt_links, k=num)
 
 def get_random_quote(num):
 	pass
