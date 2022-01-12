d325e9aa3972cfe36cb6e61bab8ba48e39c5ac02
Zeno3463 <zenolim3463@gmail.com>
2022-01-12 14:27:58 +0800

    feat: Display images and videos in the client

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,24 @@
 import type { NextPage } from 'next'
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import Image from 'next/image';
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
 
 const Home: NextPage = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+	const [index, setIndex] = useState(0);
+
 	return (
-		<div>
-			{data.map((item: any) => <div>{item}</div>)}
+		<div className='m-auto mt-10 w-fit'>
+			{data[index].includes('encrypted') ? 
+			<Image src={data[index]} width={150} height={150} /> :
+			<ReactPlayer url={data[index]} />
+			}
 		</div>
 	)
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-	const res = await fetch("http://127.0.0.1:5000/get_random/10");
+	const res = await fetch("http://127.0.0.1:5000/get_random/50");
 	const data = await res.json();
 
 	if (!data) return { notFound: true };
